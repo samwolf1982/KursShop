12a2b0f92e0ab7e5f8859f32e79a217d0c3736de
Sam <biryukwolf@yahoo.com>
2016-05-28 21:25:14 +0300

    jojo

--- views/admin/edit_product.php
+++ views/admin/edit_product.php
@@ -7,25 +7,25 @@
   //  $translate        
 
 include_once SITE_ROOT.'/views/common/header.php';
-  $translate =include (SITE_ROOT.'/components/language.php');
-  include_once SITE_ROOT.'/models/User.php';
+$translate =include (SITE_ROOT.'/components/language.php');
+include_once SITE_ROOT.'/models/User.php';
 
 
 ?>
 
-   <div id="templatemo_main">
+<div id="templatemo_main">
  
-        <div id="content" class="float_r">
-          
-            <h1><?php echo $translate['edit_product']; ?></h1>
-            <div class="content_half float_l">
+  <div id="content" class="float_r">
+    
+    <h1><?php echo $translate['edit_product']; ?></h1>
+    <div class="content_half float_l">
       
-                <div id="edit_products_admin_panel">
-                  
+      <div id="edit_products_admin_panel">
+        
 
-                  <div id="contact_form">
-                   <form method="post" name="contact" action="/contacts">
-                        
+        <div id="contact_form">
+         <form method="post" name="contact" action="/contacts">
+          
 
 <!--                         $this->id=$id;
     $this->title=$title;
@@ -38,42 +38,42 @@ include_once SITE_ROOT.'/views/common/header.php';
    
 
 
- -->
-                        
-                        <div class="cleaner h10"></div>
-                        <label for="name"><?php echo $translate['name'].':'; ?></label> <input value=<?php echo $product->get_name(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
-                        <div class="cleaner h10"></div>
-                           <label for="name"><?php echo $translate['brand'].':'; ?></label> <input value=<?php echo $product->get_brand(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
-                        <div class="cleaner h10"></div>
-                           <label for="name"><?php echo $translate['price'].':'; ?></label> <input value=<?php echo $product->get_price(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
-                        <div class="cleaner h10"></div>
-                           <label for="name"><?php echo $translate['quantity'].':'; ?></label> <input value=<?php echo $product->get_quantity(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
-                        <div class="cleaner h10"></div>
-                           <label for="name"><?php echo $translate['manufactured'].':'; ?></label> <input value=<?php echo $product->get_manufactured(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
-                        <div class="cleaner h10"></div>
-                               <label for="name"><?php echo $translate['model'].':'; ?></label> <input value=<?php echo $product->get_model(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
-                        <div class="cleaner h10"></div>
-                               <label for="name"><?php echo $translate['status'].':'; ?></label> <input value=<?php echo $product->get_status(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
-                        <div class="cleaner h10"></div>
-                               <label for="name"><?php echo $translate['is_recomendet'].':'; ?></label> <input value=<?php echo $product->get_is_recomendet(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
-                        <div class="cleaner h10"></div>
-                               <label for="name"><?php echo $translate['new_prod'].':'; ?></label> <input value=<?php echo $product->get_is_new(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
-                        <div class="cleaner h10"></div>
-                               <label for="name"><?php echo $translate['avalibily'].':'; ?></label> <input value=<?php echo $product->get_avalibily(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
-                        <div class="cleaner h10"></div>
+  -->
+  
+  <div class="cleaner h10"></div>
+  <label for="name"><?php echo $translate['name'].':'; ?></label> <input value=<?php echo $product->get_name(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
+  <div class="cleaner h10"></div>
+  <label for="name"><?php echo $translate['brand'].':'; ?></label> <input value=<?php echo $product->get_brand(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
+  <div class="cleaner h10"></div>
+  <label for="name"><?php echo $translate['price'].':'; ?></label> <input value=<?php echo $product->get_price(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
+  <div class="cleaner h10"></div>
+  <label for="name"><?php echo $translate['quantity'].':'; ?></label> <input value=<?php echo $product->get_quantity(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
+  <div class="cleaner h10"></div>
+  <label for="name"><?php echo $translate['manufactured'].':'; ?></label> <input value=<?php echo $product->get_manufactured(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
+  <div class="cleaner h10"></div>
+  <label for="name"><?php echo $translate['model'].':'; ?></label> <input value=<?php echo $product->get_model(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
+  <div class="cleaner h10"></div>
+  <label for="name"><?php echo $translate['status'].':'; ?></label> <input value=<?php echo $product->get_status(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
+  <div class="cleaner h10"></div>
+  <label for="name"><?php echo $translate['is_recomendet'].':'; ?></label> <input value=<?php echo $product->get_is_recomendet(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
+  <div class="cleaner h10"></div>
+  <label for="name"><?php echo $translate['new_prod'].':'; ?></label> <input value=<?php echo $product->get_is_new(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
+  <div class="cleaner h10"></div>
+  <label for="name"><?php echo $translate['avalibily'].':'; ?></label> <input value=<?php echo $product->get_avalibily(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
+  <div class="cleaner h10"></div>
 
-                                 <label for="name"><?php echo $translate['product_description'].':'; ?></label> <input value=<?php echo $product->get_full_content(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
-                        <div class="cleaner h10"></div>
-     
-        
-                        
-                        <input type="submit" value="<?php echo $translate['send'] ?>" id="submit" name="submit" class="submit_btn float_l" />
-            <input type="reset" value="<?php echo $translate['reset'] ?>" id="reset" name="reset" class="submit_btn float_r" />
-                        
-              </form>
-                </div>
+  <label for="name"><?php echo $translate['product_description'].':'; ?></label> <input value=<?php echo $product->get_full_content(); ?> type="text" id="name" name="name" class="validate-name required input_field" />
+  <div class="cleaner h10"></div>
+  
+  
+  
+  <input type="submit" value="<?php echo $translate['send'] ?>" id="submit" name="submit" class="submit_btn float_l" />
+  <input type="reset" value="<?php echo $translate['reset'] ?>" id="reset" name="reset" class="submit_btn float_r" />
+  
+</form>
+</div>
 
-                       
+
 <!--      <table>
 
            
@@ -93,7 +93,7 @@ include_once SITE_ROOT.'/views/common/header.php';
         </tr>
    
 
-    </table> -->
+      </table> -->
 
 
 
@@ -103,16 +103,16 @@ include_once SITE_ROOT.'/views/common/header.php';
                           <label>  <? echo   $value->get_cat_id()?></label>
                           <label >  <? echo   $value->get_price()?></label>         
                         <div class="cleaner h10"></div>
-                         <?php endforeach ?> -->
-        
-    
-                        
-            
-                </div>
+                      <?php endforeach ?> -->
+                      
+                      
+                      
+                      
+                    </div>
 
 
-            </div>
-                 <div class="content_half float_r">
+                  </div>
+                  <div class="content_half float_r">
 
  <!--       <?php foreach ($products as $key => $value): ?>
            <a href= <?php echo "/admin/product/edit/".$value->get_id(); ?> > <button><?php echo $translate['edit']; ?></button>
@@ -120,40 +120,40 @@ include_once SITE_ROOT.'/views/common/header.php';
         <a href=<?php echo "/admin/product/delete/".$value->get_id(); ?>>         <button><?php echo $translate['delete']; ?></button></a>
        <div class="cleaner h10"></div>
        
-       <?php endforeach ?> -->
+     <?php endforeach ?> -->
      
-      
+     
 
 
 
 
 
-       <?php if (isset($errors) && is_array($errors)): ?>
-                 <ul>
-                  <?php foreach ($errors as $key => $value): ?>
-                        <li><?php echo $value; ?></li>
-                  <?php endforeach ?>
+     <?php if (isset($errors) && is_array($errors)): ?>
+       <ul>
+        <?php foreach ($errors as $key => $value): ?>
+          <li><?php echo $value; ?></li>
+        <?php endforeach ?>
 
 
-                 </ul>
+      </ul>
 
-       <?php endif ?>
+    <?php endif ?>
 
-       <?php if ($result): ?>
-                <p><?php echo $translate['update_data_user']; ?></p>
-       <?php endif ?>
+    <?php if ($result): ?>
+      <p><?php echo $translate['update_data_user']; ?></p>
+    <?php endif ?>
 
 
-        </div>
-        
-        <div class="cleaner h40"></div>
-        
-        <iframe width="680" height="340" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Central+Park,+New+York,+NY,+USA&amp;aq=0&amp;sll=14.093957,1.318359&amp;sspn=69.699334,135.263672&amp;vpsrc=6&amp;ie=UTF8&amp;hq=Central+Park,+New+York,+NY,+USA&amp;ll=40.778265,-73.96988&amp;spn=0.033797,0.06403&amp;t=m&amp;output=embed"></iframe>
-            
-        </div> 
-        <div class="cleaner"></div>
-    </div> <!-- END of templatemo_main -->
-    
+  </div>
+  
+  <div class="cleaner h40"></div>
+  
+  <iframe width="680" height="340" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Central+Park,+New+York,+NY,+USA&amp;aq=0&amp;sll=14.093957,1.318359&amp;sspn=69.699334,135.263672&amp;vpsrc=6&amp;ie=UTF8&amp;hq=Central+Park,+New+York,+NY,+USA&amp;ll=40.778265,-73.96988&amp;spn=0.033797,0.06403&amp;t=m&amp;output=embed"></iframe>
+  
+</div> 
+<div class="cleaner"></div>
+</div> <!-- END of templatemo_main -->
+
 
 <?php
 include_once SITE_ROOT.'/views/common/footer.php';
