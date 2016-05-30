ac7d61897e5e3f6b36e8fd5f02ddd16000ebe42e
Sam <biryukwolf@yahoo.com>
2016-05-30 14:36:34 +0300

    dfsd

---
+++ views/layouts/footer.php
@@ -0,0 +1,20 @@
+        <footer id="footer"><!--Footer-->
+            <div class="footer-bottom">
+                <div class="container">
+                    <div class="row">
+                        <p class="pull-left">Copyright © 2016</p>
+                        <p class="pull-right">MotoOne</p>
+                    </div>
+                </div>
+            </div>
+        </footer><!--/Footer-->
+
+
+
+        <script src="/template/js/jquery.js"></script>
+        <script src="/template/js/bootstrap.min.js"></script>
+        <script src="/template/js/price-range.js"></script>
+        <script src="/template/js/jquery.prettyPhoto.js"></script>
+        <script src="/template/js/main.js"></script>
+    </body>
+</html>
